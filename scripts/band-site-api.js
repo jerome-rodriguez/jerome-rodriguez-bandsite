d877e2ae23b5224c39f334317bcf06bce1ae49c1
Jerome Rodriguez <jerome.rodriguez@torontomu.ca>
2025-01-10 18:12:38 -0500

    BandSiteApi class done + implemented try/catch

--- scripts/band-site-api.js
+++ scripts/band-site-api.js
@@ -7,28 +7,38 @@ class BandSiteApi {
 
   // sends a POST request to API
   async postComment(comment) {
-    const response = await axios.post(
-      `${this.baseUrl}/comments?api_key=${this.apiKey}`,
-      comment
-    );
-    return response.data;
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/comments?api_key=${this.apiKey}`,
+        comment
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error posting comment:", error);
+    }
   }
 
   // sends a GET request to the API and returns array from newest to oldest
   async getComments() {
-    const response = await axios.get(
-      `${this.baseUrl}/comments?api_key=${this.apiKey}`
-    );
-    return response.data.sort((a, b) => b.timestamp - a.timestamp);
+    try {
+      const response = await axios.get(
+        `${this.baseUrl}/comments?api_key=${this.apiKey}`
+      );
+      return response.data.sort((a, b) => b.timestamp - a.timestamp);
+    } catch (error) {
+      console.error("Error getting comments:", error);
+    }
   }
 
   // sends a GET request to shows API and returns array of show data objects
   async getShows() {
-    const response = await axios.get(
-      `${this.baseUrl}/showdates?api_key=${this.apiKey}`
-    );
-    return response.data;
+    try {
+      const response = await axios.get(
+        `${this.baseUrl}/showdates?api_key=${this.apiKey}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error getting shows information:", error);
+    }
   }
 }
-
-// add try-catch block, recommended when using APIs
